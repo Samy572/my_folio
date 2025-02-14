2d4b165310bdf49bd21487e28fd403ea53ef3b4c
Sam <samy_ch@outlook.fr>
2025-02-14 20:55:20 +0100

    Add tool prisma

--- src/components/Skills/getSkills/tools.ts
+++ src/components/Skills/getSkills/tools.ts
@@ -23,4 +23,12 @@ export const tools = [
 		className:
 			'inline-flex items-center justify-between rounded-md border w-fit px-2 py-0.5 text-lg font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent  shadow bg-[#646464] hover:bg-[#36363b] md:mr-4 mr-2 my-2',
 	},
+	{
+		label: 'Prisma',
+		svg: 'asset/svg/tools/prisma.svg',
+		alt: 'prisma',
+		path: 'https://www.prisma.io/',
+		className:
+			'inline-flex items-center justify-between rounded-md border w-fit px-2 py-0.5 text-lg font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent  shadow bg-[#646464] hover:bg-[#36363b] md:mr-4 mr-2 my-2',
+	},
 ];
