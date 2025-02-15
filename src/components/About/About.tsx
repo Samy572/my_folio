5444417a862dca8868d5e1e79f53acaad343b86b
Sam <samy_ch@outlook.fr>
2025-02-15 14:38:05 +0100

    Add background linear-gradient

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,7 @@ import RightSide from './RightSide/RightSide';
 
 const About = () => {
 	return (
-		<section className="  w-full h-[100vh] about mt-4 flex md:justify-between md:flex-row flex-col-reverse ">
+		<section className="about w-full h-[100vh] mt-4 flex md:justify-between md:flex-row flex-col-reverse ">
 			<div className="h-full w-full flex md:justify-between justify-center items-center pt-2 col-span-2 ">
 				<LeftSide />{' '}
 			</div>
